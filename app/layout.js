70bc3e18620d0302c66d31ad30dd983fd970ac2c
Santoshalimkar <santosh.sa4d@gmail.com>
2024-05-31 23:32:56 +0530

    changes added

--- app/layout.js
+++ app/layout.js
@@ -28,6 +28,17 @@ export default function RootLayout({ children }) {
           type="image/png"
           sizes="32x32"
         />
+
+        <meta property="og:title" content="Angadi World Technologies Pvt Ltd" />
+        <meta
+          property="og:description"
+          content="Angadi World Technologies (AWT) is the best website designing company offering top-notch mobile app development, digital marketing, project consulting, and QR code technology solutions in Bangalore, Hyderabad, India, US, and UK"
+        />
+        <meta
+          property="og:image"
+          content="https://res.cloudinary.com/dipr7y36n/image/upload/v1717178542/AWT-WEBSITE/velm49jtssv2r6hqtgyy.png"
+        />
+        <meta property="og:url" content="https://www.angadiworldtech.com" />
       </head>
       <body className={inter.className}>
         <Providers>
