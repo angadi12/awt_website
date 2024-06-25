71cd2793399d9ea58ffcb7a1264671b5208bc92f
Santoshalimkar <santosh.sa4d@gmail.com>
2024-06-25 16:50:01 +0530

    Changes added

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,8 @@ import Nav from "@/components/Navbarcomponents/Nav";
 import { Providers } from "./Provider";
 import Footer from "../components/Footercomponents/Footer";
 import FAQ from "@/components/Homecomponets/FAQ";
-import Script from 'next/script';
+import Script from "next/script";
+import GoogleAnalytics from "./GoogleAnalytics";
 
 const inter = Montserrat({ subsets: ["latin"] });
 
@@ -14,9 +15,6 @@ export const metadata = {
   description:
     "Angadi World Technologies (AWT) is the best website designing company offering top-notch mobile app development, digital marketing, project consulting, and QR code technology solutions in Bangalore, Hyderabad, India, US, and UK",
 };
-
-
-const GTM_ID = 'GTM-54SSBWG';
 
 export default function RootLayout({ children }) {
   // useEffect(() => {
@@ -30,19 +28,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-      <Script
-          id="gtm-script"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','${GTM_ID}');
-            `,
-          }}
-        />
         <link
           href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap"
           rel="stylesheet"
@@ -65,6 +50,7 @@ export default function RootLayout({ children }) {
         />
         <meta property="og:url" content="https://www.angadiworldtech.com" />
       </head>
+      <GoogleAnalytics/>
       <body className={inter.className}>
         <Providers>
           <Nav />
@@ -72,14 +58,6 @@ export default function RootLayout({ children }) {
           <FAQ />
           <Footer />
         </Providers>
-        <noscript>
-          <iframe
-            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
-            height="0"
-            width="0"
-            style={{ display: 'none', visibility: 'hidden' }}
-          />
-        </noscript>
       </body>
     </html>
   );
