a0fb87e22466330299de94579ebac6a4a9d2c55a
Santoshalimkar <santosh.sa4d@gmail.com>
2024-06-28 15:04:13 +0530

    Changes added

--- app/page.js
+++ app/page.js
@@ -17,6 +17,20 @@ import Script from "next/script";
 export default function Home() {
   return (
     <main className="w-full m-0 p-0">
+      {/* <!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/5e26c02bdaaca76c6fcf11b6/default';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script--> */}
+
       <Script
         src="https://www.kenyt.ai/botapp/ChatbotUI/dist/js/bot-loader.js"
         type="text/javascript"
--- components/Careerscomponent/Searchbar.js
+++ components/Careerscomponent/Searchbar.js
@@ -14,7 +14,7 @@ const Searchbar = () => {
             <p className='md:text-sm lg:text-sm font-medium md:leading-8 text-gray-500'>Start your career with Angadi World Technologies!</p>
            </div>
 
-           <div className='md:w-3/4 lg:w-3/4 w-11/12 gap-4 mx-auto flex justify-between items-center px-2 rounded-full boxshadow h-12'>
+           {/* <div className='md:w-3/4 lg:w-3/4 w-11/12 gap-4 mx-auto flex justify-between items-center px-2 rounded-full boxshadow h-12'>
                 <div className='flex justify-center items-center md:gap-4 lg:gap-4 gap-2 w-full'>
                     <IoSearch size={24} className='text-[#FF7143]'/>
                     <input placeholder='Search for your designation' className='w-full font-medium text-xs md:text-sm lg:text-sm capitalize outline-none'/>
@@ -24,7 +24,7 @@ const Searchbar = () => {
                     Search
                     </Button>
                 </div>
-           </div>
+           </div> */}
     </div>
   )
 }
